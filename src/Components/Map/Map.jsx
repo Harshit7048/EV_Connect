0607fa5cf1a434eb6596d9b772bfb589bfe9f322
Harshit Bhardwaj <harshit704892@gmail.com>
2025-10-06 10:12:10 +0530

    updated map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,8 +1,57 @@
+import { useCallback, useEffect, useRef } from 'react'
 import './map.css'
+import leaflet, { circle, map } from 'leaflet'
+import 'leaflet/dist/leaflet.css';
 
-export default function Map(){
-    return <div className="map-container">
-        <div className="map-main"></div>
+
+export default function Map() {
+
+    const mapRef = useRef()
+
+
+
+    useEffect(() => {
+        /// asking current location
+        navigator.geolocation.watchPosition(success);
+        function success(pros) {
+            const long = pros.coords.longitude;
+            const lat = pros.coords.latitude;
+            const accuracy = pros.coords.accuracy;
+
+            leaflet.marker([lat, long]).addTo(map);
+            let circel = leaflet.circle([lat, long], { radius: accuracy }).addTo(map)
+            map.fitBounds(circel.getBounds())
+
+
+
+        }
+
+
+
+
+        // mapRef.current = leaflet.map('map').setView([51.505, -0.09], 13);
+        const map = leaflet.map(mapRef.current).setView([51.505, -0.09], 13);
+
+        leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+
+        return () => map.remove();
+
+    }, [])
+
+
+
+
+
+
+    return <div className='map-cont'>
+        <div ref={mapRef} id="map"></div>
+
     </div>
+
+
 
 }
