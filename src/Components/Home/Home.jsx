1f0883eba8b3d3f23769d2eabf6b78980b7f1ca5
Harshit Bhardwaj <harshit704892@gmail.com>
2025-10-06 13:13:15 +0530

    message

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,60 +10,62 @@ import Footer from "../Footer/Footer";
 const results = [
     {
         pointName: " point 1",
-        pointPrice: "15/unit"
+        pointPrice: "15/unit",
+        lat: 28.752180, long: 77.143120,
     },
     {
         pointName: " point 2",
-        pointPrice: "15/unit"
+        pointPrice: "15/unit",
+        lat: 28.746950, long: 77.147320,
     },
     {
         pointName: " point 3",
-        pointPrice: "15/unit"
+        pointPrice: "15/unit",
+        lat: 28.744720, long: 77.138950,
     }
     ,
     {
         pointName: " point 4",
-        pointPrice: "15/unit"
+        pointPrice: "15/unit",
+        lat: 28.751110, long: 77.136870,
     },
     {
         pointName: " point 5",
-        pointPrice: "15/unit"
+        pointPrice: "15/unit",
+        lat: 28.747520, long: 77.142420,
     }
-    , {
-        pointName: " point 6",
-        pointPrice: "15/unit"
-    }
+
 ];
 
 
 export default function Home() {
-  const [slot , setSlot] = useState(false);
+    const [slot, setSlot] = useState(false);
 
-  //// setting states for point info
-  const [pointName , setPointName] = useState("");
-  const [pointPrice , setPointPrice] = useState(0);
-  const [pointImg , setPointImg] = useState("")
+    //// setting states for point info
+    const [pointName, setPointName] = useState("");
+    const [pointPrice, setPointPrice] = useState(0);
+    const [pointImg, setPointImg] = useState("")
 
 
     return <div className="home">
         <Filter />
-        <Map />
+        <Map points={results} />
         <Section_1 points={results} slot={slot} setSlot={setSlot}
-        setPointName={setPointName}
-        setPointPrice={setPointPrice}
-        setPointImg={setPointImg}
+            setPointName={setPointName}
+            setPointPrice={setPointPrice}
+            setPointImg={setPointImg}
         >
-        
+
 
         </Section_1>
-        {slot==true?<BookSlot
-         pointName={pointName}
-         pointPrice={pointPrice}
-         pointImg={pointImg}
-         setSlot={setSlot}
-         />:<></>}
+        {slot == true ? <BookSlot
+            pointName={pointName}
+            pointPrice={pointPrice}
+            pointImg={pointImg}
+            setSlot={setSlot}
+        /> : <></>}
 
-         <Section_2/>
-         <Footer/>
+        <Section_2 />
+        <Footer />
     </div>
 }
--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -4,33 +4,19 @@ import leaflet, { circle, map } from 'leaflet'
 import 'leaflet/dist/leaflet.css';
 
 
-export default function Map() {
+export default function Map({ points }) {
 
-    const mapRef = useRef()
+    const mapRef = useRef(null);
+    const markerRef = useRef(null)
+    const circleRef = useRef(null)
 
 
 
     useEffect(() => {
-        /// asking current location
-        navigator.geolocation.watchPosition(success);
-        function success(pros) {
-            const long = pros.coords.longitude;
-            const lat = pros.coords.latitude;
-            const accuracy = pros.coords.accuracy;
-
-            leaflet.marker([lat, long]).addTo(map);
-            let circel = leaflet.circle([lat, long], { radius: accuracy }).addTo(map)
-            map.fitBounds(circel.getBounds())
-
-
-
-        }
-
-
 
 
         // mapRef.current = leaflet.map('map').setView([51.505, -0.09], 13);
-        const map = leaflet.map(mapRef.current).setView([51.505, -0.09], 13);
+        const map = leaflet.map(mapRef.current).setView([29.74812853, 78.14084244], 13);
 
         leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -38,9 +24,49 @@ export default function Map() {
         }).addTo(map);
 
 
+
+        /// asking current location
+        navigator.geolocation.watchPosition((pros) => {
+            const long = pros.coords.longitude;
+            const lat = pros.coords.latitude;
+            const accuracy = pros.coords.accuracy;
+
+            console.log(lat, long);
+
+            if (markerRef.current) map.removeLayer(markerRef.current)
+            if (circleRef.current) map.removeLayer(circleRef.current)
+
+            let marker = leaflet.marker([lat, long]);
+            let circle = leaflet.circle([lat, long], { radius: accuracy });
+
+            marker.addTo(map);
+            circle.addTo(map);
+
+
+            markerRef.current = marker;
+            circleRef.current = circle;
+
+            map.fitBounds(circle.getBounds())
+
+
+        }, (err) => console.log(err))
+
+        const markers = points.map((p) => {
+            if (p.lat && p.long) {
+                leaflet.marker([p.lat, p.long]).addTo(map)
+            }
+        })
+
+        // const group = leaflet.featureGroup(markers)
+
+        // map.fitBounds(group.getBounds())
+
+
+
+
         return () => map.remove();
 
-    }, [])
+    }, [points])
 
 
 
